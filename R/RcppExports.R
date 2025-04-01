a70def6eb39dc826936ada98e83ef5982930ae13
Kári Hlynsson <karih@modulor.local>
2025-04-01 00:48:34 +0000

    updates to package structure

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -69,3 +69,37 @@ optim_matching <- function(sol_mat, snp_pairs, female_swap_idx, num_iterations =
     .Call(`_amsimr_optim_matching`, sol_mat, snp_pairs, female_swap_idx, num_iterations, temp_decay, init_temp, auto_temp_samples, auto_accept_ratio, collect_metrics, quietly)
 }
 
+#' Extend (or construct) the pedigree matrix for a generation
+#'
+#' Constructs a pedigree matrix for the provided generation of size (L * n)
+#' where L is the pedigree depth and n is the population size. If the current
+#' pedigree depth is L, the oldest layer is discarded. If the current pedigree
+#' is an empty matrix, initialises the pedigree.
+#'
+#' @param pedigree_current The pedigree of the current generation to extend
+#' @param matching Integer vector of length n, where matching[i] specifies the
+#'   index of the mate of individual i in the population
+#' @param pedigree_depth
+#'
+#' @return An extended pedigree for the current generation
+#'
+#' @noRd
+NULL
+
+#' Compute the pedigree distance between individuals
+#'
+#' Computes the distance between two individuals in the population pedigree
+#' graph given their index identifiers and the pedigree using the LCA algorithm
+#' for DAGs. Returns the maximum integer value if a common ancestor is not
+#' found.
+#'
+#' @param pedigree The population pedigree
+#' @param ind1_id Index of the first individual
+#' @param ind2_id Index of the second individual
+#'
+#' @return The pedigree distance between ind1 and ind2, or the maximum integer
+#'   value if no common ancestor is found.
+#'
+#' @noRd
+NULL
+
