2a4b25e8260f6f814a7e02b16bb7555b4c563413
Kári Hlynsson <karihlynsson@Modulor.local>
2025-06-20 20:08:41 +0000

    finishing genotype and phenotype generation algorithms

--- R/phenotype.R
+++ R/phenotype.R
@@ -1,17 +1,18 @@
 Phenotype <- R6::R6Class(
   "Phenotype",
+
   public = list(
     initialize = function(name, heritability, n_loci = NULL, causal_snps = NULL,
                           causal_effects = NULL) {
       checkmate::assert_string(name, null.ok = FALSE)
-      checkmate::assert_numeric(heritability, len = 1, lower = 0,
-                                upper = 1, null.ok = FALSE)
+      checkmate::assert_numeric(heritability, len = 1, lower = 0, upper = 1,
+                                null.ok = FALSE)
       if (missing(n_loci)) {
         checkmate::assert_character(causal_snps, pattern = "rs[0-9]+$")
         checkmate::assert_numeric(causal_effects, len = length(causal_snps))
-        private$.causal_snps <- causal_snps
+        private$.causal_snps    <- causal_snps
         private$.causal_effects <- causal_effects
-        private$.n_loci <- length(causal_snps)
+        private$.n_loci         <- length(causal_snps)
       } else if (missing(causal_snps) && missing(causal_effects)) {
         checkmate::assert_count(n_loci)
         private$.n_loci <- n_loci
@@ -21,33 +22,43 @@ Phenotype <- R6::R6Class(
                  `causal_snps` and `causal_effects`."
         ))
       }
-      private$.name <- name
+      private$.name         <- name
       private$.heritability <- heritability
     },
 
     # Getters
     name           = function() return(private$.name),
+    data           = function() return(private$.data),
     n_loci         = function() return(private$.n_loci),
     causal_snps    = function() return(private$.causal_snps),
     causal_effects = function() return(private$.causal_effects),
     heritability   = function() return(private$.heritability),
 
     # Setters
-    set_n_loci = function(n_loci) {
-      private$.n_loci <- n_loci
-    },
+    set_n_loci = function(n_loci) private$.n_loci <- n_loci,
     set_causal_snps = function(causal_snps) {
       private$.causal_snps <- causal_snps
     },
     set_causal_effects = function(causal_effects) {
       private$.causal_effects <- causal_effects
+    },
+
+    # Other
+    compute_data = function(snp_matrix) {
+      checkmate::assert_matrix(snp_matrix, mode = "integer",
+                               any.missing = FALSE, all.missing = FALSE,
+                               ncols = self$n_loci())
+      private$.data <- snp_matrix %*% self$causal_effects() +
+        rnorm(nrow(snp_matrix), 0, 1 - self$heritability())
     }
   ),
+
   private = list(
-    .name = NULL,
-    .n_loci = NULL,
-    .causal_snps = NULL,
+    .name           = NULL,
+    .data           = NULL,
+    .n_loci         = NULL,
+    .causal_snps    = NULL,
     .causal_effects = NULL,
-    .heritability = NULL
+    .heritability   = NULL
   )
 )
--- R/population.R
+++ R/population.R
@@ -3,7 +3,7 @@ Population <- R6::R6Class(
 
   public = list(
     initialize = function(n_population, n_loci, snp_dist = "beta",
-                          snp_params = c(1.5, 2.5), phenotypes = NULL,
+                          snp_params = c(5, 5), phenotypes = NULL,
                           mating_model = NULL) {
       checkmate::assert_count(n_population)
       checkmate::assert_count(n_loci)
@@ -12,11 +12,10 @@ Population <- R6::R6Class(
       checkmate::assert_class(phenotypes, "Phenotype", null.ok = TRUE)
       checkmate::assert_class(mating_model, "MatingModel", null.ok = TRUE)
 
-      rdist  <- get(paste0("r", snp_dist), mode = "function")
-      param1 <- snp_params[[1]]
-      param2 <- snp_params[[2]]
-
-      snp_mafs <- rdist(n_loci, param1, param2)
+      rdist           <- get(paste0("r", snp_dist), mode = "function")
+      param1          <- snp_params[[1]]
+      param2          <- snp_params[[2]]
+      snp_mafs        <- rdist(n_loci, param1, param2)
       names(snp_mafs) <- paste0("rs", 1:n_loci)
 
       checkmate::assert_numeric(snp_mafs, lower = 0, upper = 1, finite = TRUE,
@@ -41,6 +40,7 @@ Population <- R6::R6Class(
     mating_model   = function() return(private$.mating_model),
     snp_mafs       = function() return(private$.snp_mafs),
     phenotypes     = function() return(private$.phenotypes),
+    generation     = function() return(private$.generation),
 
     # Setter methods
     set_mating_model = function(mating_model) {
@@ -61,10 +61,16 @@ Population <- R6::R6Class(
       checkmate::assert_choice(phenotype_name, choices = phenotype_names)
       private$.phenotypes[[phenotype_name]] <- NULL
     },
-    compile = function() {
-      private$.setup_phenotypes()
+    compile = function(verbose = TRUE) {
+      cli::cli_h1("Compiling Population object:")
+      cli::cli_h2("Initialising population data")
       private$.initialise_data()
+      cli::cli_h2("Generating sibling pairs")
+      private$.tick_generation()
+      cli::cli_h2("Setting up phenotypes")
+      private$.setup_phenotypes()
       private$.compiled <- TRUE
+      cli::cli_alert_info("Done!")
     }
   ),
 
@@ -82,6 +88,7 @@ Population <- R6::R6Class(
     .phenotypes     = NULL,
     .mating_model   = NULL,
     .compiled       = FALSE,
+    .generation     = 0,
 
     # Utility methods
     .setup_phenotypes = function() {
@@ -93,12 +100,16 @@ Population <- R6::R6Class(
                                     number of SNPs modelled in the
                                     population."))
           }
+          sd_eff     <- sqrt(phenotype$heritability() / phenotype$n_loci())
           causal_idx <- sample(1:self$n_loci(), size = phenotype$n_loci())
-          causal_eff <- rep(phenotype$heritability() / phenotype$n_loci(),
-                            phenotype$n_loci())
+          causal_eff <- rnorm(phenotype$n_loci(), mean = 0, sd = sd_eff)
           phenotype$set_causal_snps(paste0("rs", causal_idx))
           phenotype$set_causal_effects(causal_eff)
         }
+        causal_idx  <- sort(as.integer(gsub("rs", "", phenotype$causal_snps())))
+        causal_snps <- self$genotype_data()[, causal_idx]
+        phenotype$set_causal_snps(paste0("rs", causal_idx))
+        phenotype$compute_data(causal_snps)
       }
     },
 
@@ -110,9 +121,68 @@ Population <- R6::R6Class(
       )
     },
 
-    .next_generation = function() {
-      phenotypes_filtered <- self$phenotype_data() |> filter(select ...)
-      self$mating_model()$generate_matching(phenotypes_filtered)
+    .tick_generation = function(ncores = parallel::detectCores() - 1) {
+      private$.generation <- private$.generation + 1
+      genotypes_new <- bigstatsr::FBM(
+        nrow = self$n_population(),
+        ncol = self$n_loci(),
+        type = "unsigned short",
+        backingfile = paste0("genotypes_gen", self$generation())
+      )
+
+      n_sex <- population$n_population() / 2
+      chunk_size <- 10000
+
+      # Create a list of chunks to fill the matrix
+      start_rows <- seq(1, n_sex, by = chunk_size)
+      end_rows   <- pmin(start_rows + chunk_size - 1, n_sex)
+      chunk_data <- list(start = start_rows, end = end_rows)
+
+      future::plan(future::multisession, workers = ncores)
+
+      furrr::future_walk(seq_along(chunk_data$start), ~ {
+        start_row      <- chunk_data$start[.x]
+        end_row        <- chunk_data$end[.x]
+        n_row          <- end_row - start_row + 1
+
+        row_idx_male   <- start_row:end_row
+        row_idx_female <- (n_sex + start_row):(n_sex + end_row)
+
+        chunk_male     <- population$genotype_data()[row_idx_male, ]
+        chunk_female   <- population$genotype_data()[row_idx_female, ]
+        prob_male      <- rep(as.vector(chunk_male / 2), times = 2)
+        prob_female    <- rep(as.vector(chunk_female / 2), times = 2)
+
+        male_gametes <- matrix(
+          rbinom(
+            n_row * population$n_loci() * 2,
+            size = 1,
+            prob = prob_male
+          ),
+          nrow = n_row * 2,
+          ncol = population$n_loci(),
+        )
+
+        female_gametes <- matrix(
+          rbinom(
+            n_row * population$n_loci() * 2,
+            size = 1,
+            prob = prob_female
+          ),
+          nrow = n_row * 2,
+          ncol = population$n_loci()
+        )
+
+        male_gametes_m   <- male_gametes[1:n_row, ]
+        male_gametes_f   <- female_gametes[1:n_row, ]
+        female_gametes_m <- male_gametes[(n_row + 1):(n_row * 2), ]
+        female_gametes_f <- female_gametes[(n_row + 1):(n_row * 2), ]
+
+        genotypes_new[row_idx_male, ] <- male_gametes_m + male_gametes_f
+        genotypes_new[row_idx_female, ] <- female_gametes_m + female_gametes_f
+      }, .options = furrr::furrr_options(seed = TRUE))
+      future::plan(future::sequential)
+      private$.genotype_data <- genotypes_new
     },
 
     .check_compiled = function() {
