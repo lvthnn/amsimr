47cc612d320c20ae0acae400e18572905e7e618b
lvthnn <kari.hlynsson@gmail.com>
2025-11-23 20:55:37 +0000

    fix enum conversion functions

--- src/bindings_utils.h
+++ src/bindings_utils.h
@@ -12,35 +12,43 @@
 #include <string>
 
 template<typename T>
-inline std::optional<T> _optional(SEXP x) {
+inline std::optional<T> optional(SEXP x) {
   if (x == R_NilValue) return std::nullopt;
   return Rcpp::as<T>(x);
 }
 
-inline amsim::MatingType _s_MatingType(const std::string& mating_type) {
+inline amsim::MatingType strMatingType(const std::string& mating_type) {
   if (mating_type == "random")
     return amsim::MatingType::RANDOM;
-  else if (mating_type == "assortative")
+  if (mating_type == "assortative")
     return amsim::MatingType::ASSORTATIVE;
-  else
-    Rcpp::stop("Unknown MatingType " + mating_type);
+  Rcpp::stop("Unknown MatingType " + mating_type);
 }
 
-inline amsim::ComponentType _s_ComponentType(const std::string& component_type) {
+inline amsim::ComponentType strComponentType(const std::string& component_type) {
   if (component_type == "genetic")
     return amsim::ComponentType::GENETIC;
-  else if (component_type == "environmental")
+  if (component_type == "environmental")
     return amsim::ComponentType::ENVIRONMENTAL;
-  else if (component_type == "vertical")
+  if (component_type == "vertical")
     return amsim::ComponentType::VERTICAL;
-  else if (component_type == "total")
+  if (component_type == "total")
     return amsim::ComponentType::TOTAL;
-  else
-    Rcpp::stop("Unknown ComponentType " + component_type);
+  Rcpp::stop("Unknown ComponentType " + component_type);
 }
 
-inline amsim::LogLevel _s_LogLevel(const std::string& log_level) {
-  return amsim::LogLevel::DEBUG;
+inline amsim::LogLevel strLogLevel(const std::string& log_level) {
+  if (log_level == "debug")
+    return amsim::LogLevel::DEBUG;
+  if (log_level == "info")
+    return amsim::LogLevel::INFO;
+  if (log_level == "warning")
+    return amsim::LogLevel::WARNING;
+  if (log_level == "error")
+    return amsim::LogLevel::ERROR;
+  if (log_level == "NONE")
+    return amsim::LogLevel::NONE;
+  Rcpp::stop("Unknown LogLevel " + log_level);
 }
 
 #endif // AMSIMR_BINDINGS_UTILS_H
