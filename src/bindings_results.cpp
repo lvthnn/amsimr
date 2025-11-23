3228f48b9cf6651e8afa06603bae634322b9c991
lvthnn <kari.hlynsson@gmail.com>
2025-11-23 20:55:03 +0000

    create bindings for `SimulationResults` backend class

--- src/bindings_results.cpp
+++ src/bindings_results.cpp
@@ -4,30 +4,57 @@
 
 #include "bindings_utils.h"
 
-SEXP SimulationResults__new(const std::string& output_dir) {
+//' Create a new SimulationResults object reference
+//'
+//' @return A new SimulationResults instance.
+//'
+// [[Rcpp::export(".SimulationResults_new")]]
+SEXP SimulationResults_new(const std::string& output_dir) {
   auto results = std::make_unique<amsim::SimulationResults>(output_dir);
   Rcpp::XPtr<amsim::SimulationResults> ptr(results.release(), true);
   return ptr;
 }
 
-void SimulationResults__summarise(SEXP ptr) {
+//' Perform summarisation on replicate metric data
+//'
+//' @param ptr An external pointer to a SimulationResults instance.
+//'
+// [[Rcpp::export(".SimulationResults_summarise")]]
+void SimulationResults_summarise(SEXP ptr) {
   Rcpp::XPtr<amsim::SimulationResults> results(ptr);
   results->summarise();
 }
 
-void SimulationResults__save(
+//' Save summarised results to disk
+//'
+//' @param ptr An external pointer to a SimulationResults instance.
+//' @param metrics A list of metric names to be saved to file.
+//' @param out_dir Output directory to write data out to.
+//' @param overwrite Specifies whether directory should be overwritten.
+//'
+// [[Rcpp::export(".SimulationResults_save")]]
+void SimulationResults_save(
     SEXP ptr,
     SEXP metrics = R_NilValue,
     SEXP out_dir = R_NilValue,
     bool overwrite = false) {
   Rcpp::XPtr<amsim::SimulationResults> results(ptr);
   results->save(
-      _optional<std::vector<std::string>>(metrics),
-      _optional<std::string>(out_dir),
+      optional<std::vector<std::string>>(metrics),
+      optional<std::string>(out_dir),
       overwrite);
 }
 
-Rcpp::DataFrame SimulationResults__load(
+//' Load a summarised metric into the binding layer.
+//'
+//' @param ptr An external pointer to a SimulationResults instance.
+//' @param metric_name Name of the metric to be loaded into the environment.
+//'
+//' @return A DataFrame comprising the summarised results data for the specified
+//'   metric.
+//'
+// [[Rcpp::export(".SimulationResults_load")]]
+Rcpp::DataFrame SimulationResults_load(
     SEXP ptr, const std::string& metric_name) {
   Rcpp::XPtr<amsim::SimulationResults> results(ptr);
   Rcpp::DataFrame metric_results;
@@ -40,7 +67,7 @@ Rcpp::DataFrame SimulationResults__load(
 
   for (std::size_t col = 0; col < n_cols; ++col) {
     for (std::size_t row = 0; row < n_rows; ++row) {
-      std::size_t idx = row * n_cols + col;
+      std::size_t idx = (row * n_cols) + col;
       index[idx] = row + 1;
       names[idx] = table.inv_label_map[col];
     }
@@ -56,10 +83,18 @@ Rcpp::DataFrame SimulationResults__load(
   metric_results["upper_ci95"] = table.data[5];
   metric_results["quant_025"] = table.data[6];
   metric_results["quant_975"] = table.data[7];
+
   return metric_results;
 }
 
-std::vector<std::string> SimulationResults__metric_names(SEXP ptr) {
+//' Get metric names
+//'
+//' @param ptr An external pointer to a SimulationResults instance.
+//'
+//' @return A vector of the metric names recognised by the results instance.
+//'
+// [[Rcpp::export(".SimulationResults_metric_names")]]
+std::vector<std::string> SimulationResults_metric_names(SEXP ptr) {
   Rcpp::XPtr<amsim::SimulationResults> results(ptr);
   return results->metric_names();
 }
